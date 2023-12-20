b3ef6abffdf75ad37aef5e61b896426a6dc6c843
s22013 <s22013@std.it-college.ac.jp>
2023-12-20 13:45:21 +0900

    ログイン画面と新規登録画面の修正

--- pages/login.js
+++ pages/login.js
@@ -11,12 +11,13 @@ import {
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { onAuthStateChanged } from "firebase/auth"; // 追加
+import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "firebase/auth"; // signInWithPopupとGoogleAuthProviderを追加
 import { useRouter } from "next/router";
 import { useAuth, useUser } from "../hooks/firebase.js";
 import Link from "next/link";
 import style from "../styles/signup.module.css";
 import Footer from "../components/Footer.js";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -35,22 +36,36 @@ export default function Login() {
     } catch (error) {
       console.error("Login error:", error.message);
       setErrorMessage("ログインに失敗しました。ユーザー名とパスワードを確認してください。");
-      setIsProcessingLogin(false); // ログイン失敗時に isProcessingLogin を false に戻す
-      alert("ログインに失敗しました")
+      setIsProcessingLogin(false);
+      alert("ログインに失敗しました");
     }
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setErrorMessage(""); // フォームが再送信されたときにエラーメッセージをリセット
-    setIsProcessingLogin(true); // ログインボタンを無効化
+    setErrorMessage("");
+    setIsProcessingLogin(true);
     login();
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      setIsProcessingLogin(true);
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log("Googleログイン成功:", user);
+      setIsProcessingLogin(false);
+    } catch (error) {
+      console.error("Googleログインエラー:", error.message);
+      setIsProcessingLogin(false);
+      alert("ログインに失敗しました");
+    }
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        // ログイン成功したらホームにリダイレクト
         router.push("/");
       }
     });
@@ -63,102 +78,120 @@ export default function Login() {
   return (
     <div className={style.mainContainer}>
       <main>
-    <Flex
-      minHeight="100vh"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Box
-        w="60%"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-      >
-        <Heading color="gray.800" mb="60px" textAlign="center" size="2xl">
-          ログイン
-        </Heading>
-        <Box
-          boxShadow="lg"
-          w="700px"
-          paddingY="140px"
-          paddingX="48px"
-          borderRadius="8px"
-          border="1px solid"
-          borderColor="gray.100"
-          m="0 auto"
-          display="flex"
-          flexDirection="column"
+        <Flex
+          minHeight="100vh"
           alignItems="center"
+          justifyContent="center"
         >
-          <form onSubmit={handleLogin}>
-            <Flex flexDirection="column" mb="30" alignItems="center">
-              <FormLabel fontWeight="bold" mb="2">
-                Email
-              </FormLabel>
-              <Input
-                type="email"
-                size="lg"
-                borderRadius="md"
-                placeholder="example@test.com"
-                width="100%"
-                height="30px"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Flex>
-            <Flex flexDirection="column" mb="50" alignItems="center">
-              <FormLabel fontWeight="bold" mb="2">
-                パスワード
-              </FormLabel>
-              <Flex alignItems="center">
-                <Input
-                  type={isPasswordVisible ? "text" : "password"}
-                  size="lg"
-                  borderRadius="md"
-                  width="300px"
-                  height="30px"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <IconButton
-                  icon={isPasswordVisible ? <ViewOffIcon /> : <ViewIcon />}
-                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                  aria-label={
-                    isPasswordVisible ? "Hide Password" : "Show Password"
-                  }
-                  size="lg"
-                  mb="2"
-                />
-              </Flex>
-            </Flex>
-
-            <Flex flexDirection="column" alignItems="center">
-              <Button className={style.touroku}
-                type="submit"             
-                size="lg"
-                paddingX="80px"
-                m="0 auto"
-                isLoading={isProcessingLogin}
-                _hover={{
-                  background: "gray.700",
-                }}
-              >
+          <Box
+            w="60%"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Box
+              boxShadow="lg"
+              w="500px"
+              paddingY="60px"
+              paddingX="20px"
+              borderRadius="4px"
+              border="1px solid"
+              borderColor="gray.100"
+              m="0 auto"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              bg="white"
+            >
+              <Heading color="gray.800" mb="60px" textAlign="center" size="2xl" marginBottom="38px">
                 ログイン
-              </Button>
-            </Flex>
-          </form>
-        </Box>
-        <Text mt="4" textAlign="center">
-          新規ユーザーですか？{" "}
-          <Link legacyBehavior href="/signup">
-            <a style={{ color: 'blue' }}>新規登録はこちら</a>
-          </Link>
-        </Text>
-      </Box>
-    </Flex>
-    </main>
-    <Footer />
+              </Heading>
+              <form onSubmit={handleLogin}>
+                <Flex flexDirection="column" mb="30" alignItems="center">
+                  <FormLabel fontWeight="bold" mb="2">
+                    Email
+                  </FormLabel>
+                  <Input
+                    type="email"
+                    size="lg"
+                    borderRadius="md"
+                    placeholder="example@test.com"
+                    width="100%"
+                    height="30px"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Flex>
+                <Flex flexDirection="column" mb="50" alignItems="center">
+                  <FormLabel fontWeight="bold" mb="2">
+                    パスワード
+                  </FormLabel>
+                  <Flex alignItems="center">
+                    <Input
+                      type={isPasswordVisible ? "text" : "password"}
+                      size="lg"
+                      borderRadius="md"
+                      width="300px"
+                      height="30px"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <IconButton
+                      icon={isPasswordVisible ? <ViewOffIcon /> : <ViewIcon />}
+                      onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                      aria-label={
+                        isPasswordVisible ? "Hide Password" : "Show Password"
+                      }
+                      size="lg"
+                      mb="2"
+                    />
+                  </Flex>
+                </Flex>
+
+                <Flex flexDirection="column" alignItems="center">
+                  <Button
+                    className={style.touroku}
+                    type="submit"
+                    size="lg"
+                    paddingX="80px"
+                    m="0 auto"
+                    isLoading={isProcessingLogin}
+                    _hover={{
+                      background: "gray.700",
+                    }}
+                  >
+                    ログイン
+                  </Button>
+                  <h3>または</h3>
+                  <Button
+                    className={style.google}
+                    type="button"
+                    size="lg"
+                    paddingX="80px"
+                    m="0 auto"
+                    mt="4"
+                    isLoading={isProcessingLogin}
+                    _hover={{
+                      background: "gray.700",
+                    }}
+                    onClick={handleGoogleLogin}
+                  >
+                    Googleでログイン
+                  </Button>
+                </Flex>
+              </form>
+
+              <Text mt="4" textAlign="center">
+                新規ユーザーですか？{" "}
+                <Link legacyBehavior href="/signup">
+                  <a style={{ color: 'blue' }}>新規登録はこちら</a>
+                </Link>
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+      </main>
+      <Footer />
     </div>
   );
 }
-
--- pages/signup.js
+++ pages/signup.js
@@ -16,9 +16,9 @@ import { useForm } from "react-hook-form";
 import { useAuth, useUser } from "../hooks/firebase.js";
 import Link from "next/link";
 import style from "../styles/signup.module.css";
-import { signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { FaGoogle } from "react-icons/fa";
-import Footer  from "@/components/Footer.js";
+import Footer from "@/components/Footer.js";
 
 export default function Signup() {
   const {
@@ -63,200 +63,197 @@ export default function Signup() {
     console.log("currentUser:", currentUser);
     console.log("pathname:", router.pathname);
 
-    // サインアップが完了しているかどうかを最初に確認
     if (!isProcessingSignup && currentUser && router.pathname !== "/") {
       console.log("Redirecting to /");
       router.push("/");
     }
   }, [currentUser, isProcessingSignup, router]);
+
   const googleProvider = new GoogleAuthProvider();
   const handleGoogleLogin = async () => {
     try {
       setIsProcessingSignup(true);
       const result = await signInWithPopup(auth, googleProvider);
       const user = result.user;
-      // 必要に応じてユーザーデータを処理できます
       console.log("Googleログイン成功:", user);
       setIsProcessingSignup(false);
-
-      // ログインが成功した場合、ユーザーがすでに存在しないか確認し、新規登録するなどの処理を追加できます
     } catch (error) {
       console.error("Googleログインエラー:", error.message);
       setIsProcessingSignup(false);
-      alert("ログインに失敗しました")
+      alert("ログインに失敗しました");
     }
   };
+
   return (
     <div className={style.mainContainer}>
-    <main> 
-    <Flex
-      minHeight="100vh"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Box
-        w="60%"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-      >
-        <Heading color="gray.800" mb="60px" textAlign="center" size="2xl">
-          新規登録
-        </Heading>
-        <Box
-          boxShadow="lg"
-          w="700px"
-          paddingY="100px"
-          paddingX="48px"
-          borderRadius="8px"
-          border="1px solid"
-          borderColor="gray.100"
-          m="0 auto"
-          display="flex"
-          flexDirection="column"
+      <main>
+        <Flex
+          minHeight="100vh"
           alignItems="center"
+          justifyContent="center"
         >
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Flex flexDirection="column" mb="25" alignItems="center">
-              <FormLabel fontWeight="bold" mb="2">
-                Email
-              </FormLabel>
-              {errors.email && (
-                <Text color="red.400" mb="2">
-                 <font color="red"> Emailが入力されていません</font>
-                </Text>
-              )}
-              <Input
-                type="email"
-                size="2lg"
-                borderRadius="md"
-                placeholder="example@test.com"
-                {...register("email", { required: true })}
-                width="100%"
-                height="30px"
-              />
-            </Flex>
-            <Flex flexDirection="column" mb="25" alignItems="center">
-              <FormLabel fontWeight="bold" mb="2">
-                パスワード
-              </FormLabel>
-              {errors.password && (
-                <Text color="red.400" mb="2">
-                  <font color="red">パスワードが入力されていません</font>
-                </Text>
-              )}
-              <Flex alignItems="center">
-                <Input
-                  type={isPasswordVisible ? "text" : "password"}
-                  {...register("password", { required: true })}
-                  size="lg"
-                  borderRadius="md"
-                  height="30px"
-                  width="300px"
-                />
-                <IconButton
-                  icon={isPasswordVisible ? <ViewOffIcon /> : <ViewIcon />}
-                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                  aria-label={
-                    isPasswordVisible ? "Hide Password" : "Show Password"
-                  }
-                  size="lg"
-                  mb="2"
-                />
-              </Flex>
-            </Flex>
-            <Flex flexDirection="column" mb="40" alignItems="center">
-              <FormLabel fontWeight="bold" mb="2">
-                パスワード(再確認)
-              </FormLabel>
-              {errors.confirmationPassword && (
-                <Text color="red.400" mb="2">
-                 <font color="red">パスワード(再確認)が入力されていません</font>
-                </Text>
-              )}
-              <Flex alignItems="center">
-                <Input
-                  type={isConfirmPasswordVisible ? "text" : "password"}
-                  {...register("confirmationPassword", { required: true })}
-                  size="lg"
-                  borderRadius="md"
-                  width="300px"
-                  height="30px"
-                />
-                <IconButton
-                  icon={
-                    isConfirmPasswordVisible ? <ViewOffIcon /> : <ViewIcon />
-                  }
-                  onClick={() =>
-                    setIsConfirmPasswordVisible(!isConfirmPasswordVisible)
-                  }
-                  aria-label={
-                    isConfirmPasswordVisible
-                      ? "Hide Confirm Password"
-                      : "Show Confirm Password"
-                  }
-                  size="lg"
-                  mb="2"
-                />
-              </Flex>
-            </Flex>
-
-            <Flex flexDirection="column" alignItems="center">
-          
-              <Button className={style.touroku} 
-                type="submit"
-                color="black"
-                background="gray.800"
-                size="lg"
-                paddingX="60px"
-                m="0 auto"
-                marginBottom="30"
-                isLoading={isProcessingSignup}
-                _hover={{
-                  background: "gray.700",
-                }}
-              >
-                登録
-              </Button>
-          
-      <h3> または</h3>
-              <Button
-          className={style.google}
-          type="button"
-          color="black"
-          background="gray.800"
-          size="lg"
-          paddingX="80px"
-          m="0 auto"
-          marginBottom="4"
-          isLoading={isProcessingSignup}
-          _hover={{
-            background: "gray.700",
-          }}
-          onClick={handleGoogleLogin}
-        >
-          <Flex align="center">
-            <IconButton
-              icon={<FaGoogle />}
-              fontSize="20px"
-              marginRight="2"  
-            />
-            Googleで登録・ログイン
-          </Flex>
-        </Button>
-              <Text mb="8" textAlign="center">
-                すでにアカウントをお持ちですか?{" "}
-                <Link legacyBehavior href="/login">
-                  <a style={{ color: 'blue' }}>ログイン</a>
-                </Link>
-              </Text>
-              </Flex>
-          </form>
-        </Box>
-      </Box>
-    </Flex>
-    </main> 
-    <Footer/>
+          <Box
+            w="60%"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Box
+              boxShadow="lg"
+              w="600px"
+              paddingTop="40px"
+              paddingBottom="32px"
+              borderRadius="8px"
+              border="1px solid"
+              borderColor="gray.100"
+              m="0 auto"
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              bg="white" // ここで背景色を指定
+            >
+              <Heading color="gray.800" mb="60px" textAlign="center" size="2xl">
+                新規登録
+              </Heading>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Flex flexDirection="column" mb="25" alignItems="center">
+                  <FormLabel fontWeight="bold" mb="2">
+                    Email
+                  </FormLabel>
+                  {errors.email && (
+                    <Text color="red.400" mb="2">
+                      <font color="red"> Emailが入力されていません</font>
+                    </Text>
+                  )}
+                  <Input
+                    type="email"
+                    size="2lg"
+                    borderRadius="md"
+                    placeholder="example@test.com"
+                    {...register("email", { required: true })}
+                    width="100%"
+                    height="30px"
+                  />
+                </Flex>
+                <Flex flexDirection="column" mb="25" alignItems="center">
+                  <FormLabel fontWeight="bold" mb="2">
+                    パスワード
+                  </FormLabel>
+                  {errors.password && (
+                    <Text color="red.400" mb="2">
+                      <font color="red">パスワードが入力されていません</font>
+                    </Text>
+                  )}
+                  <Flex alignItems="center">
+                    <Input
+                      type={isPasswordVisible ? "text" : "password"}
+                      {...register("password", { required: true })}
+                      size="lg"
+                      borderRadius="md"
+                      height="30px"
+                      width="300px"
+                    />
+                    <IconButton
+                      icon={isPasswordVisible ? <ViewOffIcon /> : <ViewIcon />}
+                      onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                      aria-label={
+                        isPasswordVisible ? "Hide Password" : "Show Password"
+                      }
+                      size="lg"
+                      mb="2"
+                    />
+                  </Flex>
+                </Flex>
+                <Flex flexDirection="column" mb="40" alignItems="center">
+                  <FormLabel fontWeight="bold" mb="2">
+                    パスワード(再確認)
+                  </FormLabel>
+                  {errors.confirmationPassword && (
+                    <Text color="red.400" mb="2">
+                      <font color="red">パスワード(再確認)が入力されていません</font>
+                    </Text>
+                  )}
+                  <Flex alignItems="center">
+                    <Input
+                      type={isConfirmPasswordVisible ? "text" : "password"}
+                      {...register("confirmationPassword", { required: true })}
+                      size="lg"
+                      borderRadius="md"
+                      width="300px"
+                      height="30px"
+                    />
+                    <IconButton
+                      icon={
+                        isConfirmPasswordVisible ? <ViewOffIcon /> : <ViewIcon />
+                      }
+                      onClick={() =>
+                        setIsConfirmPasswordVisible(!isConfirmPasswordVisible)
+                      }
+                      aria-label={
+                        isConfirmPasswordVisible
+                          ? "Hide Confirm Password"
+                          : "Show Confirm Password"
+                      }
+                      size="lg"
+                      mb="2"
+                    />
+                  </Flex>
+                </Flex>
+                <Flex flexDirection="column" alignItems="center">
+                  <Button
+                    className={style.touroku}
+                    type="submit"
+                    color="black"
+                    background="gray.800"
+                    size="lg"
+                    paddingX="60px"
+                    m="0 auto"
+                    marginBottom="30"
+                    isLoading={isProcessingSignup}
+                    _hover={{
+                      background: "gray.700",
+                    }}
+                  >
+                    登録
+                  </Button>
+                  <h3>または</h3>
+                  <Button
+                    className={style.google}
+                    type="button"
+                    color="black"
+                    background="gray.800"
+                    size="lg"
+                    paddingX="80px"
+                    m="0 auto"
+                    marginBottom="4"
+                    isLoading={isProcessingSignup}
+                    _hover={{
+                      background: "gray.700",
+                    }}
+                    onClick={handleGoogleLogin}
+                  >
+                    <Flex align="center">
+                      <IconButton
+                        icon={<FaGoogle />}
+                        fontSize="20px"
+                        marginRight="2"
+                      />
+                      Googleで登録・ログイン
+                    </Flex>
+                  </Button>
+                  <Text mb="8" textAlign="center">
+                    すでにアカウントをお持ちですか?{" "}
+                    <Link legacyBehavior href="/login">
+                      <a style={{ color: 'blue' }}>ログイン</a>
+                    </Link>
+                  </Text>
+                </Flex>
+              </form>
+            </Box>
+          </Box>
+        </Flex>
+      </main>
+      <Footer />
     </div>
   );
 }
