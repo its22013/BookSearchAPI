501886dedc764434b4505058f43cdd82e5328033
s22012 <s22012@std.it-college.ac.jp>
2023-12-21 12:44:22 +0900

    ランキング作成

--- pages/Ranking.js
+++ pages/Ranking.js
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from '../styles/Ranking.module.css';
+import Header from '../components/HeaderSigup';
 
 const RankingPage = () => {
   const [rankings, setRankings] = useState([]);
@@ -7,9 +10,8 @@ const RankingPage = () => {
   useEffect(() => {
     const fetchRankings = async () => {
       try {
-        const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+        const apiKey = process.env.APP_ID;
 
-        // sort パラメータに sales を指定して売れている順にランキングを取得
         const response = await axios.get(
           `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&sort=sales&applicationId=${apiKey}&hits=20`
         );
@@ -25,18 +27,35 @@ const RankingPage = () => {
 
   return (
     <div>
-      <h1>ランキングページ</h1>
-      {rankings.length > 0 ? (
-        <ul>
-          {rankings.map((book, index) => (
-            <li key={book.Item.itemCode}>
-              {index + 1}. {book.Item.title}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>ランキング情報がありません。</p>
-      )}
+      <Header />
+      <main>
+        <div className={styles.container}>
+          <h1>月間売上ランキング</h1>
+          {rankings.length > 0 ? (
+            <ul>
+              {rankings.map((book, index) => (
+                <li key={book.Item.itemCode} className={styles.listItem}>
+                  <div className={styles.rank}>{index + 1} 位</div>
+                  <div className={styles.content}>
+                    <span className={styles.title}>{book.Item.title}</span>
+                    <img
+                      src={book.Item.mediumImageUrl}
+                      alt={book.Item.title}
+                    />
+                  </div>
+                  <p className={styles.p}>
+                    <Link legacyBehavior href={book.Item.itemUrl}>
+                      <a target="_blank" rel="noopener noreferrer">詳細・購入</a>
+                    </Link>
+                  </p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>ランキング情報がありません。</p>
+          )}
+        </div>
+      </main>
     </div>
   );
 };
